f6a2a4a5dcac83821f1c09fcc78a95167d795c83
prince404B <praj81232@gmail.com>
2025-02-01 21:20:47 +0530

    Section: Education & Experience added

--- components/Background/Exp_Card.jsx
+++ components/Background/Exp_Card.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import CardLayout from '../Common/CardLayout'
-
+import { SquareArrowOutUpRight } from "lucide-react"
 const Exp_Card = ({ data }) => {
     return (
-        <CardLayout>
-            <div className="card_stylings transition px-8 py-10 ">
-                <div className="flex-initial text-[17px] text-Snow font-medium">{data.title}</div>
-                <div className="text-sm text-LightGray font-normal italic mt-1 ">
-                    {data.role}
-                </div>
-                <div className="text-LightGray text-xs opacity-50 font-normal italic">
-                    <a href={`${data.url}`} target="_blank" rel="noreferrer">
-                        {data.url}
-                    </a>
-                </div>
-                <div className="text-LightGray text-sm font-normal mt-4 text-justify">
-                    {data.desc}
-                </div>
-                <div className="flex justify-between text-LightGray bg-DeepNightBlack w-full h-auto text-xs rounded-full py-3 px-6 mt-4 font-normal opacity-50">
-                    <div>{data.year}</div> <div>{data.location}</div>
-                </div>
-            </div>
-        </CardLayout>
-    )
+      <CardLayout>
+        <div className="card_stylings transition px-8 py-10 ">
+          <div className="flex-initial items-start flex text-[17px] text-Snow font-medium">
+            {data.title}
+            <a href={`${data.url}`} target="_blank" rel="noreferrer">
+              <SquareArrowOutUpRight className="ml-2" size={20} />
+            </a>
+          </div>
+          <div className="text-sm text-LightGray font-normal italic mt-1 ">
+            {data.role}
+          </div>
+          <div className="text-LightGray text-sm font-normal mt-4">
+            {data.desc.split("\n").map((line, index) => (
+              <p key={index} className="mb-2">
+                {line}
+              </p>
+            ))}
+          </div>
+
+          <div className="flex justify-between text-LightGray bg-DeepNightBlack w-full h-auto text-xs rounded-full py-3 px-6 mt-4 font-normal opacity-50">
+            <div>{data.year}</div> <div>{data.location}</div>
+          </div>
+        </div>
+      </CardLayout>
+    );
 }
 
 export default Exp_Card
--- pages/api/background.js
+++ pages/api/background.js
@@ -1,64 +1,65 @@
 const background = [
-    {
-        eduCards: [
-            {
-                id: 0,
-                title: 'UET Peshawar',
-                degree: 'BS, Computer System Engineering',
-                detail: "Bachelor's Degree in Computer System Engineering from UET Peshawar.",
-                year: '2018-2022'
-            },
-            {
-                id: 1,
-                title: 'Peshawar Model Degree College',
-                degree: 'HSSC, Pre Engineering',
-                detail: "Completed FSC part 1 and part 2 in Pre-Engineering from Peshawar Model Degree College.",
-                year: '2015-2017'
-            },
-            {
-                id: 2,
-                title: 'Falcon Academy',
-                degree: 'SSC, Science Subjects',
-                detail: "Completed SSC part 1 and part 2 in Science subjects from Falcon Academy Seni Gumbat Kohat",
-                year: '2013-2015'
-            },
-        ]
-    },
-    {
-        expCards: [
-            {
-                id: 1,
-                title: 'JMM Technologies',
-                role: 'Frontend Developer',
-                url: 'https://jmm.ltd/',
-                desc: 'As a frontend developer, I use React, Next & JavaScript to build user interfaces for web applications.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
-            },
-            {
-                id: 2,
-                title: 'HauzaTech',
-                role: 'Internee',
-                url: 'no website',
-                desc: 'As an Internee, I learned how to use React & JavaScript to build interactive websites.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
-            },
-            {
-                id: 3,
-                title: 'Encoder Bytes',
-                role: 'PHP Developer',
-                url: 'https://www.encoderbytes.com/',
-                desc: "I work there as a PHP developer, there I learned how to do CRUD'S operations in PHP, also I worked on Firebase",
-                year: '09/2020 - 02/2021',
-                location: 'Peshawar, Pakistan'
-            },
+  {
+    eduCards: [
+      {
+        id: 0,
+        title: "Galgotias University",
+        degree: "B.Tech in Computer Science and Engineering",
+        grade: "8.4 CGPA",
+        detail:
+          "Pursuing a Bachelor's degree in Computer Science and Engineering at Galgotias University, focusing on software development, data structures, algorithms, and modern web technologies.",
+        year: "Aug 2022 - Sept 2026",
+      },
+      {
+        id: 1,
+        title: "New Lancer Convent School",
+        degree: "12th Board ",
+        grade: "92%",
+        detail:
+          "Completed senior secondary education with a focus on Physics, Chemistry, and Mathematics",
+        year: "Feb 2020 - Mar 2022",
+      },
+    ],
+  },
+  {
+    expCards: [
+      {
+        id: 1,
+        title: "DevTown",
+        role: "Software Developer",
+        url: "https://www.devtown.in/",
+        desc: "• Developed and integrated APIs to enable seamless frontend-backend communication while implementing secure user authentication mechanisms.\n• Led a cost-saving initiative by identifying unused AWS resources and establishing S3 bucket expiration policies, leading to an annual cost reduction exceeding $50,000 in AWS expenditures.\n• Utilized AWS (EC2, S3), ReactJS, TypeScript, PostgreSQL, NeonDB, Migrations, and secure authentication techniques to optimize system performance and scalability, improving operational efficiency by 40%.",
+        year: "Oct 2024 - Present",
+        location: "Maharashtra, India",
+      },
 
-        ]
-    }
-]
+      {
+        id: 2,
+        title: "Xipper",
+        role: "Full Stack Developer",
+        url: "https://xipper.in/",
+        desc: `• Designed and integrated RESTful APIs, reducing system overload and enhancing communication between frontend and backend systems by 30%, ensuring robust and scalable interactions.\n
+    • Designed scalable real-time communication features using WebSocket, ensuring smooth and reliable user experiences in live chat applications, reducing latency issues by 35%.\n
+    • ReactJS, TypeScript, Lambda, Firebase, OTPLess, MongoDB, React Native, Tailwind CSS, Shadcn.`,
+        year: "Aug 2024 - Oct 2024",
+        location: "Bangalore, India",
+      },
 
+      {
+        id: 3,
+        title: "EliteKods",
+        role: "Software Developer",
+        url: "https://elitekods.com/",
+        desc: `• Optimized payment gateway integration, resulting in a 25% reduction in transaction failures and improved user trust.\n
+    • Developed and integrated mobile applications, ensuring seamless functionality, intuitive interfaces, and smooth performance across devices, reducing user complaints by 60%.\n
+    • JavaScript, React Native, NextJS, Redux, AWS, S3, Docker, Material-UI, Payment Gateway, WebSocket.`,
+        year: "Jan 2024 - Aug 2024",
+        location: "Pune, India",
+      },
+    ],
+  },
+];
 
 export default function handler(req, res) {
-    res.status(200).json(background)
+  res.status(200).json(background);
 }
