cdd016179961bfa188c87acf1a9a570bbf6064c6
prince404B <praj81232@gmail.com>
2025-02-06 03:17:25 +0530

    adding favicon.ico

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Software Developer | Portfolio</title>
+      <link rel="icon" href="/favicon.png"/>
+      <meta name="description" content="Software Developer Portfolio" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
