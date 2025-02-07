8425ef61577dfee0dd9ee9ecf7a8027a16925e92
Prince Raj <127084297+HardCoder404@users.noreply.github.com>
2025-02-07 19:52:29 +0530

    Update Banner.jsx

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -19,8 +19,8 @@ const Banner = () => {
                                             <Typewriter
                                                 options={{
                                                     strings:
-                                                        ['MERN Stack Developer',
-                                                            'NextJs Developer'],
+                                                        [
+                                                            'Software Developer'],
                                                     autoStart: true,
                                                     loop: true,
                                                 }}
