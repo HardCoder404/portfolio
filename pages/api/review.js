e77b5a4f5def94a7b35b3673a55974d2b05b8166
Prince Raj <127084297+HardCoder404@users.noreply.github.com>
2025-02-07 22:42:53 +0530

    Update review.js

--- pages/api/review.js
+++ pages/api/review.js
@@ -1,15 +1,15 @@
 const review = [
   {
     id: 0,
-    clientName: "Mustafash",
-    clientLocation: "Egypt",
+    clientName: "Matthew",
+    clientLocation: "US",
     clientSource: "Fiverr",
     clientReview:
       "It’s always a pleasure working with him. He completes tasks efficiently and goes above and beyond expectations. His deep knowledge of his work is evident, and I look forward to our next collaboration."  },
   {
     id: 2,
-    clientName: "Melissabrown255",
-    clientLocation: "United States",
+    clientName: "Kartikey",
+    clientLocation: "Pune, India",
     clientSource: "Fiverr",
     clientReview:
       "He is an excellent communicator and an experienced web developer. It was a great experience working with him. ",
@@ -17,7 +17,7 @@ const review = [
   {
     id: 3,
     clientName: "Jake Corry",
-    clientLocation: "Germany",
+    clientLocation: "US",
     clientSource: "Fiverr",
     clientReview:
       "Prince is extremely knowledgeable and helpful. He has an in-depth understanding of web app development and a strong grasp of various technologies and frameworks. His expertise is truly impressive, and I hope to have the opportunity to work with him again.",
@@ -25,7 +25,7 @@ const review = [
   {
     id: 4,
     clientName: "Blaise Labriola",
-    clientLocation: "France",
+    clientLocation: "Germany",
     clientSource: "Fiverr",
     clientReview:
       "I have worked with Prince, and he is a highly professional and excellent software developer. I admire his development speed and the quality of his work when given an assignment.",
